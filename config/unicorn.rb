5b451e8f8531e7a59961be6d6837341422e41fac
Roman Kolesnev <rvkolesnev@gmail.com>
2012-05-12 16:51:27 +0400

    switch to sockets

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -27,8 +27,8 @@ working_directory app_path # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-#listen "/tmp/.sock", :backlog => 64
-listen 8080, :tcp_nopush => true
+listen app_path + "shared/unicorn.sock", :backlog => 64
+#listen 8080, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
