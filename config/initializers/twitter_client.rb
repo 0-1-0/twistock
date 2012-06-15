f8bf7745eefa68977b3f609c08dd1bfe979f1a56
Nickolay Yegorov <nickolay@yegorov.info>
2012-06-16 02:45:33 +0400

    added apigee

--- config/initializers/twitter_client.rb
+++ config/initializers/twitter_client.rb
@@ -1,14 +1,14 @@
-if ENV['APIGEE_TWITTER_API_ENDPOINT']
-  @@twitter_api = ENV['APIGEE_TWITTER_API_ENDPOINT']
-else
+#if ENV['APIGEE_TWITTER_API_ENDPOINT']
+#  @@twitter_api = ENV['APIGEE_TWITTER_API_ENDPOINT']
+#else
   # Get this value from Heroku.
   # Once you have enabled the addon, boot up the 'heroku console' and run the following:
   # puts ENV['APIGEE_TWITTER_API_ENDPOINT']
   # this will spit out your correct api endpoint
-  @@twitter_api = "twitter-api-app4901698.apigee.com"
-end
+#  @@twitter_api = "twitter-api-app4901698.apigee.com"
+#end
 
-@endpoint = 'http://' + @@twitter_api
+#@endpoint = 'http://' + @@twitter_api
 
 
 Twitter.configure do |config|
@@ -16,5 +16,5 @@ Twitter.configure do |config|
   config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
   config.oauth_token = ENV['TWITTER_OAUTH_TOKEN']
   config.oauth_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
-  config.endpoint = @endpoint
+  #config.endpoint = @endpoint
 end
