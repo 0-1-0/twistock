c6a39bc008348ed33afc5940e6796d7c9bfde6b7
Nickolay Yegorov <nickolay@yegorov.info>
2012-06-15 17:06:10 +0400

    fixed sell button

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -18,13 +18,13 @@ class SharesController < ApplicationController
     p = params[:sell]
     owner = User.find_by_nickname(p[:owner])
     count = p[:count].to_i
-    profile = User.find_by_nickname(p[:profile])
+    user_page = User.find_by_nickname(p[:user_page])
 
     begin
       current_user.sell_shares owner, count
-      redirect_to profile_path(profile), notice: 'Success!'
+      redirect_to profile_path(user_page), notice: 'Success!'
     rescue
-      redirect_to profile_path(profile), alert: $!.to_s
+      redirect_to profile_path(user_page), alert: $!.to_s
     end
   end
 
