31fcd07efbc60edc166df795e1b497183433d80c
Nickolay Yegorov <nickolay@yegorov.info>
2012-07-16 21:44:03 +0400

    fixed configuration files + @user.share_price

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,13 +5,13 @@ timeout 30
 preload_app true
 
 
-listen "/home/twitterexchange/twitterexchange/tmp/sockets/unicorn.sock", :backlog => 64
+listen "/home/twitterexchange/twitterexchange/shared/unicorn.sock", :backlog => 64
 
-pid "//home/twitterexchange/twitterexchange/tmp/pids/unicorn.pid"
+pid "/home/twitterexchange/twitterexchange/shared/pids/unicorn.pid"
 
 # Set the path of the log files inside the log folder of the testapp
-stderr_path "/home/twitterexchange/twitterexchange/log/unicorn.stderr.log"
-stdout_path "/home/twitterexchange/twitterexchange/log/unicorn.stdout.log"
+stderr_path "/home/twitterexchange/twitterexchange/shared/log/unicorn.stderr.log"
+stdout_path "/home/twitterexchange/twitterexchange/shared/log/unicorn.stdout.log"
 
 before_fork do |server, worker|
   # Replace with MongoDB or whatever
