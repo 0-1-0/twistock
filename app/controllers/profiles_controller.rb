c294b9d3b768ac040c492d6104d0ceeb91313bba
Nickolay Yegorov <nickolay@yegorov.info>
2012-07-17 06:48:03 +0400

    updated profiles show view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,6 +5,8 @@ class ProfilesController < ApplicationController
     unless @user = ( User.find_by_nickname(params[:id]) or User.create_from_twitter(params[:id]) )
       return redirect_to not_found_path
     end
+
+
     @my_page  = (@user == current_user)
 
     #Определяем популярность пользователя
