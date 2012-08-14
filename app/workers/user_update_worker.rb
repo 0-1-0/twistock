e5c984ee80e72bfe58679b236205ee2db5b95c50
Nickolay Yegorov <nickolay@yegorov.info>
2012-08-15 02:57:18 +0400

    added twitter client randomization

--- app/workers/user_update_worker.rb
+++ app/workers/user_update_worker.rb
@@ -4,12 +4,17 @@ class UserUpdateWorker
 
   def perform(nickname)
     user = User.find_by_nickname(nickname)
+
+    #выбираем первого попавшегося и используем его твиттер для скачивания данных
+    rand_user = User.where('secret is Not NULL').first(:order=>'RANDOM()')
+    twitter = rand_user.twitter
+
     time_gate = Time.now - 20.days
 
     retweets, tweets, followers = 0, 0, 0
     begin
-      followers     = Twitter.user(nickname).followers_count
-      timeline      = Twitter.user_timeline(nickname, include_rts: 0, count: 200).select{|t| t.created_at > time_gate}
+      followers     = twitter.user(nickname).followers_count
+      timeline      = twitter.user_timeline(nickname, include_rts: 0, count: 200).select{|t| t.created_at > time_gate}
       tweets        = timeline.count
       retweets      = timeline.inject(0){|a, b| a += b.retweet_count}
     rescue
