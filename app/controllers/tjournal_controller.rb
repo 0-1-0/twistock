13937be835632111855d3d90793b0794d48b9e2c
Nickolay Yegorov <nickolay@yegorov.info>
2012-10-02 19:42:12 +0400

    redesigned top-tweet page

--- app/controllers/tjournal_controller.rb
+++ app/controllers/tjournal_controller.rb
@@ -2,6 +2,9 @@ class TjournalController < ApplicationController
   layout  'tjournal'
 
   def top_tweets
+    @users_with_top_tweets = User.where('best_tweet_retweets_num > 100').order('best_tweet_param DESC').limit(64)
+  end
 
+  def admin
   end
 end
