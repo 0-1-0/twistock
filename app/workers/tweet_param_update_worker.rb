e255da53836b43a4113d095cd7d70eaf5c39183e
Nickolay Yegorov <nickolay@yegorov.info>
2012-10-02 23:19:13 +0400

    quickfix

---
+++ app/workers/tweet_param_update_worker.rb
@@ -0,0 +1,18 @@
+class TweetParamUpdateWorker
+  include Sidekiq::Worker
+
+  def perform(id)
+    user = User.find(id)
+    if user.best_tweet_retweets_num > 0 and user.followers_num > 0
+      user.best_tweet_param = user.best_tweet_retweets_num.to_f/(user.followers_num.to_f)
+      logger.info 'processed param: ' + user.best_tweet_param.to_s
+    else
+      user.best_tweet_retweets_num = -1
+      logger.info 'Fail'
+    end
+
+    user.save
+  end
+
+
+end
