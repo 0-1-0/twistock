ed0c8caa514e6a0befb985144b5bf75cf92ccae1
Nickolay Yegorov <nickolay@yegorov.info>
2012-11-13 15:54:26 +0400

    Added Blog Controller

---
+++ app/controllers/blog_controller.rb
@@ -0,0 +1,22 @@
+class BlogController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
