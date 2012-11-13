b5254badc71d6cd33cb083a48b7521c3fb6f8db0
Nickolay Yegorov <nickolay@yegorov.info>
2012-11-13 16:23:54 +0400

    removed useless css, js and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Twitterexchange::Application.routes.draw do
   resources :product
   resources :store, only: [:index, :show]
 
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+
   resources :user, only: [:show] do
     collection do
       post 'set_mail'
@@ -30,59 +33,9 @@ Twitterexchange::Application.routes.draw do
 
   resources :tag, only: [:index, :destroy, :create, :update]
 
-
-  get "settings/twitter_translation"
-
-  resources :main_page_streams
-
-  get "robot/dashboard"
-
-  get "robot/history"
-  post 'robot/add'
-
-  get "history/transactions"
-
-  get "history_controller/operations"
-
-
-  match '/products/showcase', :controller => 'products', :action => 'showcase'
-  resources :products
-
-  require 'sidekiq/web'
-  mount Sidekiq::Web => '/sidekiq'
-
-  root to: 'welcome#index'
-  get 'not_found' => 'welcome#not_found'
-  get 'thanks' => 'welcome#thanks'
-  get 'mobile' => 'welcome#mobile'
+  root to: 'main#index'
 
   match '/auth/:provider/callback', :to => 'session#create'
   match '/auth/failure',            :to => 'session#failure'
   get 'sign_out'  => 'session#destroy'
-
-  resources :profiles, only: [:show] do
-    collection do
-      post 'search'
-      post 'show'
-      get  'change_language'
-      post 'price_after_transaction'
-    end
-  end
-
-  resources :shares, only: [] do
-    resources :profiles, only: [:show]
-    collection do
-      post 'buy'
-      post 'sell'
-      post 'sell_retention'
-      post 'buy_followers'
-    end
-  end
-
-  resources :product_invoices, only: [:create, :index]
-  match '/product_invoices/create', :to => 'product_invoices#create'
-
-  match '/history', :to => 'history#transactions'
-  match '/iportfolio', :to => 'history#investment_portfolio'
-  match '/holders', :to => 'history#share_holders'
 end
