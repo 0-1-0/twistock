241a1ca20a81029ef1d14bfbddee7c3c56e87f0b
Nickolay Yegorov <nickolay@yegorov.info>
2012-12-04 20:32:57 +0400

    added basic howitworks window

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -84,6 +84,33 @@ $(document).ready(function(){
 
 });
 
+// Show Help
+var help_block = $('#help').css('height', $(document).outerHeight() ),
+    inner_block = help_block.find('.six').css( 'height', $(window).height() );
+
+$(window).resize(function(){
+    help_block.css('height', $(document).outerHeight() );
+    inner_block.css( 'height', $(window).height() );
+});
+
+$('#show_help').on('click', function() {
+    // $('body').css('overflow', 'hidden');
+    
+    help_block.fadeIn(160,function(){
+        $(this).find('*').fadeIn(250);
+    })
+    .on('click', function(){
+        $(this).fadeOut(160).find('*').fadeOut(100);
+        $('body').css('overflow', 'auto');
+    });
+
+    return false;
+});
+
+
+
+
+
 $(document).ready(function(){
     $('#tiles').imagesLoaded(function() {
         var options = {
