4dc233b6eef03838e0322e427f1386795142a47d
Roman Kolesnev <rvkolesnev@gmail.com>
2013-01-08 09:34:29 +0400

    [#42112977]

--- app/models/user.rb
+++ app/models/user.rb
@@ -104,7 +104,8 @@ class User < ActiveRecord::Base
     end
 
     def gen_price_fluctuation(delta = 3)
-      User.all.each do |u|
+      User.select(:id).all.map(&:id).each do |id|
+        u = User.find(id)
         if u.base_price
           u.base_price = [u.base_price + rand(2*delta+1) - delta, 1].max
           u.update_share_price
